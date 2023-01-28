e29cc9b30a6244728a09e9013941ca5067f3b184
Titto <titto.punnoose12316@gmail.com>
2023-01-28 01:32:57 -0800

    3rd change

---
+++ script2.sh
@@ -0,0 +1,9 @@
+#Check whether they are equal
+if [ $1 == "beginner" ]
+then
+	    echo "entered value is beginner"
+    else
+	      echo "entered value is not beginner, it is actually $1"
+fi
+
+I an making some changes
